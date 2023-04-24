5d76986da0128a52519c93569fa4bb3bede6d574
Ziad-Nasser <ziadnasser177@gmail.com>
2023-04-24 22:25:42 +0200

    dybala

--- _printf.c
+++ _printf.c
@@ -1,6 +1,50 @@
-#include <unistd.h>
-#include <string.h>
 #include "main.h"
+#include <string.h>
+#include <unistd.h>
+/**
+*print_string - a function that writes a string to the standard output
+ *@l:variadic arguments
+ *Return:the number of characters printed
+*/
+int print_string(va_list l)
+{
+	int len;
+	char *str;
+
+	str = va_arg(l, char*);
+	if (str == NULL)
+		str = "(null)";
+	len = strlen(str);
+	print(STDOUT_FILENO, str, len);
+
+	return (len);
+}
+/**
+ *print_char - a function that writes a char to the standard output
+ *@l:variadic arguments
+ *Return:the number of characters printed
+*/
+int print_char(va_list l)
+{
+	char c;
+
+	c = (char) va_arg(l, int);
+	write(STDOUT_FILENO, &c, 1);
+
+	return (1);
+}
+/**
+ *print_specifier - a function that writes a specifier to the standard output
+ *@l:variadic arguments
+ *Return:the number of characters printed
+*/
+int print_specifier(va_list l)
+{
+	(void) l;
+	write(STDOUT_FILENO, "%", 1);
+
+	return (1);
+}
 /**
  * is_spec - checks if a character is a format specifier
  * @spacifiers: an array of specifier_t structs
@@ -18,14 +62,14 @@ int is_spec(specifier_t spacifiers[], char *format)
 		if (format[i + 1] == '\0')
 			return (-1);
 		ch = format[i + 1];
-		/*to select spacifier*/
-		for (i = 0; i < 10; i++)
+
+		for (i = 0; i < 12; i++)
 		{
 			if (spacifiers[i].s == ch)
 				return (i);
 		}
 	}
-	/*not found*/
+
 	return (-2);
 }
 /**
@@ -35,17 +79,18 @@ int is_spec(specifier_t spacifiers[], char *format)
  */
 int _printf(const char *format, ...)
 {
-	int i, s_input, print_count = 0;
+	int i, s_input, print_count;
 	va_list l;
 	specifier_t spacifiers[] = {
-	{'s', print_string}, {'c', print_char}, {'%', print_percent}
-	, {'d', print_int}, {'i', print_int}, {'b', print_binary}
-	, {NULL, NULL}};
+	  {'s', print_string}, {'c', print_char}
+	, {'%', print_specifier}, {'d', print_int}
+	, {'i', print_int}, {'b', print_binary}};
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(l, format);
+	print_count = 0;
 	for (i = 0; (format != NULL) && (format[i] != '\0'); i++)
 	{
 		s_input = is_specifier(spacifiers, (char *) (format + i));
